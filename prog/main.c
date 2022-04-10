30e7a93aaa7b198968d9121a15028df0b0845375
Seb <seebeesbt@gmail.com>
2022-04-10 20:22:12 +0100

    Shronked down the demo program

--- prog/main.c
+++ prog/main.c
@@ -1,12 +1,5 @@
-#include <stdint.h>
-#include "../drivers/pit.h"
-#include "../drivers/kbd.h"
-#include "../drivers/monitor_mode_txt.h"
-#include "../kernel/util.h"
-#include "../kernel/kernel.h"
-#include "../kernel/stdbool.h"
-#include "../kernel/util.h"
 #include "main.h"
+#include "../kernel/util.h"
 
 int run_program(void)
 {
