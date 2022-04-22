88b3b8d1042c2ae2f23327d8a91852095320a410
Seb <seebeesbt@gmail.com>
2022-04-22 12:54:26 +0100

    Made the keyboard maps constants

--- kernel/keyboard_map.h
+++ kernel/keyboard_map.h
@@ -3,10 +3,10 @@
 #define MAP_SIZE 48
 #define SHIFT_MAP_SIZE 47
 
-extern int key_codes[MAP_SIZE];
-extern char key_values[MAP_SIZE];
+extern const int key_codes[MAP_SIZE];
+extern const char key_values[MAP_SIZE];
 
-extern int shift_key_codes[SHIFT_MAP_SIZE];
-extern char shift_key_values[SHIFT_MAP_SIZE];
+extern const int shift_key_codes[SHIFT_MAP_SIZE];
+extern const char shift_key_values[SHIFT_MAP_SIZE];
 
 char get_key_from_code(int code, bool shiftPressed);
