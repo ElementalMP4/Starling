5a30fefb274ba09c4d1bab8e7eaa96b36a250e58
Seb <seebeesbt@gmail.com>
2022-04-22 21:29:53 +0100

    Changed the example program.

--- prog/print_wee/print_wee.c
+++ prog/print_wee/print_wee.c
@@ -5,9 +5,37 @@
 
 #include "print_wee.h"
 
+void print_wee() {
+	int indent = 0;
+	bool falling = false;
+
+	while (true) {
+		for (int i = 0; i <= 15; i++) {
+			set_foreground_colour(i);
+			for (int j = 0; j <= indent; j++) {
+				print(" ");
+			}
+			
+			print("00000000000000000000\n");
+			
+			if (falling) {
+				indent--;
+				if (indent == 0) falling = false;
+			} else {
+				indent++;
+				if (indent == 4) falling = true;
+			}
+
+			sleep(10);
+		}
+	}	
+}
+
 void do_the_printing() {
 	print("What is your name you buffoon: ");
 	char* name = read();
 	print("\nWow what a smelly name, ");
 	print(name);
+	sleep(2000);
+	print_wee();
 }
