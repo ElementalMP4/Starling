e1419c61dbe9349b3c0fdd8ab7fee32653cb2ca6
Seb <seebeesbt@gmail.com>
2022-04-28 12:21:29 +0100

    Added a large blue dodo splash screen

--- prog/main.c
+++ prog/main.c
@@ -1,11 +1,11 @@
 #include <starlib.h>
 #include <stdint.h>
+#include <display.h>
 #include "main.h"
 
 #include "dicegame/dicegame.h"
 
 int run_program(void)
 {
-	print("\fC\b6Hello World!\n");
-	//run_game();
+	run_game();
 }
