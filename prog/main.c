77252dda3610bc5ffa0b0974eebdaf307eccd838
Seb <seebeesbt@gmail.com>
2022-05-04 18:22:37 +0100

    Indeed

--- prog/main.c
+++ prog/main.c
@@ -4,18 +4,20 @@
 #include "main.h"
 
 void print_white_screen() {
-	for (int i = 0; i < MAX_ROWS; i++) {
-		for (int j = 0; j < MAX_COLS; j++) {
+	for (int i = 0; i < MAX_COLS; i++) {
+		for (int j = 0; j < MAX_ROWS; j++) {
 			set_background_colour(15);
-			print(" ");
+			print_at(" ", i, j);
+			sleep(1);
 		}
 	}
 }
 
+int colour = 0;
+
 void change_random_byte() {
 	int column = rand_range(0, MAX_COLS - 1);
 	int row = rand_range(0, MAX_ROWS - 1);
-	int colour = rand_range(0, 14);
 	char byte = create_attribute_byte(0, colour);
 	set_attribute_byte_at(byte, row, column);
 }
@@ -24,7 +26,11 @@ int run_program(void)
 {
 	print_white_screen();
 	while(true) {
-		change_random_byte();
-		sleep(5);
+		for (int k = 0; k < 2000; k++) {
+			change_random_byte();
+			sleep(1);
+		}
+		if (colour < 15) colour++;
+		else colour = 0;
 	}
 }
